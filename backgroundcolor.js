b530fbfaca38b9266d8529bb5848e59a286d4186
Goudmant <yvan-goudmant@gmail.com>
2021-02-26 09:30:14 +0100

    ..

--- backgroundcolor.js
+++ backgroundcolor.js
@@ -23,24 +23,24 @@
 //addFilledRect(0, 0, 800, 800);
 
 ///////////////////TEST2
-//function randomColor(){
-//    var color = "#";
-//    var randomHex = "123456ABCDEF";  
-//    for(var i = 0; i<6;i++){
-//        color+= randomHex[Math.floor(Math.random()*40)]
-//    }
-//
-//    return color;
-//}
-//
-//var mytimer ;
-//
-//function setColor(){
-//    
-//    $("canvas").css("background-color", randomColor);
-//}
-//
-//var mytimer = setInterval(setColor, .1);
+// function randomColor(){
+//     var color = "#";
+//     var randomHex = "123456ABCDEF";  
+//     for(var i = 0; i<6;i++){
+//         color+= randomHex[Math.floor(Math.random()*35)]
+//     }
+
+//     return color;
+// }
+
+// var mytimer ;
+
+// function setColor(){
+    
+//     $("canvas").css("background-color", randomColor);
+// }
+
+// var mytimer = setInterval(setColor, .1000);
 
 ///////////////////TEST3
 
